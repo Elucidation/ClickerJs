373f844dca1f7496c11710812a57d29791fae089
Sam Ansari <elucidation@gmail.com>
2015-12-26 21:35:11 -0800

    Basic deploy script for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Start from master branch
+git checkout master
+
+# Create fresh dist/ folder
+rm -rf dist/
+
+# Run build
+npm run build
+
+# Move everything to dist/
+rsync -r app/public/ dist/
+
+# Switch to gh-pages branch
+git checkout gh-pages
+
+# Move everything from dist/ delete everything except dist & excluded
+rsync --delete-after --recursive \
+      --exclude=node_modules \
+      --exclude=.git \
+      --exclude=.gitignore \
+      dist/ .
