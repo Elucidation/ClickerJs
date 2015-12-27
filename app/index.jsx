9cc1ef949060976c176fc5d43b2d4f087e41d6f1
Sam Ansari <elucidation@gmail.com>
2015-12-27 00:35:48 -0800

    Created autoclicker game, several buttons in buttons.json

--- app/index.jsx
+++ app/index.jsx
@@ -3,27 +3,30 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var React = require('react');
+var buttonUpgradeFullList = require('./upgrades.json');
 
+// Autoclick update rate in milliseconds
+var autoclickUpdateRate = 20; // 20 ~ 50Hz
 
-var buttons = [
-{id: 1, label: 'AutoClicker', ratepersecond:'1', minclicks:'10'},
-{id: 2, label: 'SuperClicker', ratepersecond:'5', minclicks:'100'},
-{id: 3, label: 'UltraClicker', ratepersecond:'20', minclicks:'1000'},
-];
+console.log(buttonUpgradeFullList);
 
 // Shows current number of clicks
 var ClickCounter = React.createClass({
   render: function() {
     return (
-      <p>
-        Clicks: {this.props.clicks}
-      </p>
+      <div>
+        <p>
+          Clicks: {Math.floor(this.props.clicks)}
+          <br/>
+          Autoclicking at {this.props.clickDelta.toFixed(0)} clicks per second (cps).
+        </p>
+      </div>
     );
   }
 });
 
-// Click button contains button for clicking and upgrades
-var ClickButtonList = React.createClass({
+// Click button contains button for clicking manually
+var ClickButton = React.createClass({
   render: function() {
     return (
       <button type="button" onClick={this.props.submitClick}>Click me</button>
@@ -31,27 +34,115 @@ var ClickButtonList = React.createClass({
   }
 });
 
+var UpgradeButton = React.createClass({
+  render: function() {
+    var upgrade = this.props.data;
+    var id = upgrade.id;
+    var state = this.props.state; // overall state, to determine if button is grayed out and what level
+    var numUpgraded = state.upgrades[id];
+    var isDisabled = state.clicks < upgrade.minclicks;
+    return (
+      <button type="button" 
+              onClick={this.props.submitUpgrade} 
+              key={id}
+              disabled={isDisabled}>{upgrade.label} ({upgrade.ratepersecond} cps) x {numUpgraded}</button>
+    );
+  }
+});
+
+//<button type="button" onClick={submitUpgradeHandler.bind(null,upgrade.id)} key={upgrade.id}>{upgrade.label}</button>
+// Upgrade auto clickers based on button selected
+var ClickButtonUpgrades = React.createClass({
+  render: function() {
+    var submitUpgradeHandler = this.props.submitUpgrade;
+    var state = this.props.state;
+
+    var upgradeButtons = this.props.upgradeFullList.map(function(upgrade,i) {
+      // Only show button if number of clicks hits min threshold
+      if (state.upgrades[i] > 0 || state.clicks >= upgrade.minclicks)
+      {
+        return (
+          <li key={upgrade.id}>
+            <UpgradeButton key={upgrade.id}
+                           submitUpgrade={submitUpgradeHandler.bind(null,upgrade.id)}
+                           data={upgrade}
+                           state={state} />
+          </li>
+        );
+      }
+
+      else
+      {
+        return;
+      }
+    });
+
+    return (
+      <div className="UpgradeList">
+        <ul>
+          {upgradeButtons}
+        </ul>
+      </div>
+    );
+  }
+});
+
 var ClickerWindow = React.createClass({
   getInitialState: function() {
-    return {clicks: 0}
+    var upgradeCounts = Array.apply(null, Array(this.props.upgradeFullList.length)).map(Number.prototype.valueOf,0);
+    return {clicks: 0, 
+            upgrades: upgradeCounts};
+  },
+
+  componentDidMount: function() {
+    setInterval(this.updateClicks, this.props.updateInterval);
+  },
+
+  updateClicks: function() {
+    // The meat, increment clicks based on autoclickers
+    var clicks = this.state.clicks;
+    var rate_multiplier = this.props.updateInterval/1000;
+
+    for (var i = 0; i < this.props.upgradeFullList.length; i++) {
+      var ratepersecond = this.props.upgradeFullList[i].ratepersecond;
+      // Add to clicks for each upgrade of this level that exists
+      clicks = clicks + ratepersecond * this.state.upgrades[i] * rate_multiplier;
+    };
+    this.setState({'clicks': clicks});
   },
 
   handleClick: function() {
     this.setState({'clicks': this.state.clicks+1})
-    console.log("Click! #"+this.state.clicks);
+  },
+
+  handleUpgrades: function(buttonId) {
+    var clicks = this.state.clicks;
+    var currentUpgrades = this.state.upgrades;
+    currentUpgrades[buttonId] = currentUpgrades[buttonId] + 1;
+    clicks = clicks - this.props.upgradeFullList[buttonId].minclicks;
+    this.setState({'clicks':clicks, 'upgrades':currentUpgrades});
   },
   render: function() {
+    var upgradeState = this.state.upgrades;
+
+    var delta = this.props.upgradeFullList.map(function(upgrade,i) {
+      return upgrade.ratepersecond * upgradeState[i];
+    }).reduce(function(a1,a2) {return a1+a2;});
+    
     return (
         <div className="clickerWindow">
           <h1>Clicker Game</h1>
-          <ClickCounter clicks={this.state.clicks} />
-          <ClickButtonList submitClick={this.handleClick} />
+          <ClickCounter clicks={this.state.clicks} clickDelta={delta} />
+          <ClickButton submitClick={this.handleClick} />
+          <ClickButtonUpgrades upgradeFullList={this.props.upgradeFullList}
+                               state={this.state}
+                               submitUpgrade={this.handleUpgrades} />
         </div>
       );
   }
 });
 
 ReactDOM.render(
-  <ClickerWindow/>,
+  <ClickerWindow upgradeFullList={buttonUpgradeFullList} updateInterval={autoclickUpdateRate}/>,
   document.getElementById('content')
 );
